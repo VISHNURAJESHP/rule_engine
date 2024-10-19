28c1c15cc6cdc861be5de94c5f14d7677baa1e1b
vishnu rajesh <vishnudxz381@gmail.com>
2024-10-20 04:07:01 +0530

    feat:add readme and fix errors

--- rule_engine/rule/urls.py
+++ rule_engine/rule/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import create_rule, evaluate_rule, combine_rule, get_rules
+from .views import create_rule, evaluate_rule, combine_rule, get_rules,delete_rule
 
 urlpatterns = [
     path('create_rule/', create_rule, name='create_rule'),
     path('evaluate_rule/', evaluate_rule, name='evaluate_rule'),
     path('combine_rules/', combine_rule, name='combine_rules'),
     path('get_rules/', get_rules, name='get_rules'), 
+    path('delete_rule/', delete_rule, name='delete_rule'),
 ]
